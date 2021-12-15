f3901132bd25ceb2df8c35f4b1a5f98eb0879a93
Rohan CJ <rohantmp@gmail.com>
2021-12-15 23:07:28 +0530

    feat: switch to namespaced watches
    Signed-off-by: Rohan CJ <rohantmp@gmail.com>

--- main.go
+++ main.go
@@ -48,23 +48,28 @@ func init() {
 	//+kubebuilder:scaffold:scheme
 }
 
+var metricsAddr string
+var enableLeaderElection bool
+var probeAddr string
+var developmentMode bool
+
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
+	flag.BoolVar(&developmentMode, "development", false, "enable to enable development")
+	opts := zap.Options{}
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
+	opts.Development = developmentMode
+	// opts.BindFlags(flag.CommandLine)
+	// flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
+	setupLog.V(1).Info("development mode on, debug logs enabled")
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
@@ -80,8 +85,9 @@ func main() {
 	}
 
 	if err = (&controllers.LVMClusterReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:          mgr.GetClient(),
+		Scheme:          mgr.GetScheme(),
+		DevelopmentMode: developmentMode,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "LVMCluster")
 		os.Exit(1)
