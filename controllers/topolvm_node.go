4f02932f9cc270f2e6bcefb74e4383cf2bc20ea6
Juan Miguel Olmo Martínez <jolmomar@redhat.com>
2021-12-20 12:39:46 +0100

    feat: implementation of topolvm csi node daemonset
    Topolvm csi node is a daemonset deploying lvmd, topolvm node csi, csiregistrar
    and a liveness probe container.
    Uses an init container for wait deployment until lvmd config file is available
    
    Signed-off-by: Juan Miguel Olmo Martínez <jolmomar@redhat.com>

---
+++ controllers/topolvm_node.go
@@ -0,0 +1,369 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	lvmv1alpha1 "github.com/red-hat-storage/lvm-operator/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"path/filepath"
+	cutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"strings"
+)
+
+const (
+	topolvmNodeName = "topolvm-node"
+)
+
+type topolvmNode struct{}
+
+func (n topolvmNode) getName() string {
+	return topolvmNodeName
+}
+
+//+kubebuilder:rbac:groups=apps,resources=daemonsets,verbs=create;update;delete;get;list;watch
+
+func (n topolvmNode) ensureCreated(r *LVMClusterReconciler, ctx context.Context, lvmCluster *lvmv1alpha1.LVMCluster) error {
+	nodeDaemonSet := getNodeDaemonSet(lvmCluster)
+	//TODO: Use the mutate function to manage the changes in the CR (nodes, lvmd config)
+	result, err := cutil.CreateOrUpdate(ctx, r.Client, nodeDaemonSet, func() error { return nil })
+
+	if err != nil {
+		r.Log.Error(err, fmt.Sprintf("%s reconcile failure", topolvmNodeName), "name", nodeDaemonSet.Name)
+	} else {
+		r.Log.Info(topolvmNodeName, "operation", result, "name", nodeDaemonSet.Name)
+	}
+	return err
+}
+
+// ensureDeleted should wait for the resources to be cleaned up
+func (n topolvmNode) ensureDeleted(r *LVMClusterReconciler, ctx context.Context, lvmCluster *lvmv1alpha1.LVMCluster) error {
+	NodeDaemonSet := &appsv1.DaemonSet{}
+	err := r.Client.Get(ctx,
+		types.NamespacedName{Name: TopolvmNodeDaemonsetName, Namespace: lvmCluster.Namespace},
+		NodeDaemonSet)
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			r.Log.Info("topolvm node deleted", "TopolvmNode", NodeDaemonSet.Name)
+			return nil
+		}
+		r.Log.Error(err, "failed to retrieve topolvm node daemonset", "TopolvmNode", NodeDaemonSet.Name)
+		return err
+	} else {
+		// if not deleted, initiate deletion
+		if NodeDaemonSet.GetDeletionTimestamp().IsZero() {
+			if err = r.Client.Delete(ctx, NodeDaemonSet); err != nil {
+				r.Log.Error(err, "failed to delete topolvm node daemonset", "TopolvmNodeName", TopolvmNodeDaemonsetName)
+				return err
+			} else {
+				// set deletion in-progress for next reconcile to confirm deletion
+				return fmt.Errorf("topolvm csi node daemonset %s is already marked for deletion", TopolvmNodeDaemonsetName)
+			}
+		}
+	}
+
+	return nil
+}
+
+// updateStatus should optionally update the CR's status about the health of the managed resource
+// each unit will have updateStatus called individually so
+// avoid status fields like lastHeartbeatTime and have a
+// status that changes only when the operands change.
+func (n topolvmNode) updateStatus(r *LVMClusterReconciler, ctx context.Context, lvmCluster *lvmv1alpha1.LVMCluster) error {
+	return nil
+}
+
+func extractNodeSelectorAndTolerations(lvmCluster lvmv1alpha1.LVMCluster) (*corev1.NodeSelector, []corev1.Toleration) {
+	// TODO: use the function defined in vgmanager.go ( lest se if we can move the function to common utils location)
+	return nil, nil
+}
+
+func getNodeDaemonSet(lvmCluster *lvmv1alpha1.LVMCluster) *v1.DaemonSet {
+	hostPathDirectory := corev1.HostPathDirectory
+	hostPathDirectoryOrCreateType := corev1.HostPathDirectoryOrCreate
+	storageMedium := corev1.StorageMediumMemory
+
+	volumes := []corev1.Volume{
+		{Name: "registration-dir",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: fmt.Sprintf("%splugins_registry/", getAbsoluteKubeletPath(CSIKubeletRootDir)),
+					Type: &hostPathDirectory}}},
+		{Name: "node-plugin-dir",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: fmt.Sprintf("%splugins/topolvm.cybozu.com/node", getAbsoluteKubeletPath(CSIKubeletRootDir)),
+					Type: &hostPathDirectoryOrCreateType}}},
+		{Name: "csi-plugin-dir",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: fmt.Sprintf("%splugins/kubernetes.io/csi", getAbsoluteKubeletPath(CSIKubeletRootDir)),
+					Type: &hostPathDirectoryOrCreateType}}},
+		{Name: "pod-volumes-dir",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: fmt.Sprintf("%spods/", getAbsoluteKubeletPath(CSIKubeletRootDir)),
+					Type: &hostPathDirectoryOrCreateType}}},
+		{Name: "lvmd-config-dir",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: filepath.Dir(lvmdConfigFile),
+					Type: &hostPathDirectory}}},
+		{Name: "lvmd-socket-dir",
+			VolumeSource: corev1.VolumeSource{
+				EmptyDir: &corev1.EmptyDirVolumeSource{Medium: storageMedium}}},
+	}
+
+	initContainers := []corev1.Container{*getNodeInitContainer()}
+	containers := []corev1.Container{*getLvmdContainer(), *getNodeContainer(), *getCsiRegistrarContainer(), *getNodeLivenessProbeContainer()}
+
+	// Affinity and tolerations
+	nodeSelector, tolerations := extractNodeSelectorAndTolerations(*lvmCluster)
+	topolvmNodeAffinity := &corev1.Affinity{}
+	if nodeSelector != nil {
+		topolvmNodeAffinity = &corev1.Affinity{
+			NodeAffinity: &corev1.NodeAffinity{RequiredDuringSchedulingIgnoredDuringExecution: nodeSelector},
+		}
+	}
+	topolvmNodeTolerations := []corev1.Toleration{{Operator: corev1.TolerationOpExists}}
+	if tolerations != nil {
+		topolvmNodeTolerations = tolerations
+	}
+
+	nodeDaemonSet := &v1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      TopolvmNodeDaemonsetName,
+			Namespace: lvmCluster.Namespace,
+		},
+		Spec: v1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app.kubernetes.io/name": lvmCluster.Name,
+				},
+			},
+			UpdateStrategy: v1.DaemonSetUpdateStrategy{
+				Type: v1.RollingUpdateDaemonSetStrategyType,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: lvmCluster.Name,
+					Labels: map[string]string{
+						"app.kubernetes.io/name": lvmCluster.Name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: TopolvmNodeServiceAccount,
+					InitContainers:     initContainers,
+					Containers:         containers,
+					Volumes:            volumes,
+					HostPID:            true,
+					Tolerations:        topolvmNodeTolerations,
+					Affinity:           topolvmNodeAffinity,
+				},
+			},
+		},
+	}
+
+	return nodeDaemonSet
+}
+
+func getNodeInitContainer() *corev1.Container {
+	command := []string{
+		"sh",
+		"-c",
+		fmt.Sprintf("until [ -f %s ]; do echo waiting for lvmd config file; sleep 5; done", lvmdConfigFile),
+	}
+
+	volumeMounts := []corev1.VolumeMount{
+		{Name: "lvmd-config-dir", MountPath: "/etc/topolvm"}}
+
+	fileChecker := &corev1.Container{
+		Name:         "file-checker",
+		Image:        auxImage,
+		Command:      command,
+		VolumeMounts: volumeMounts,
+	}
+
+	return fileChecker
+}
+
+func getLvmdContainer() *corev1.Container {
+	command := []string{
+		"/lvmd",
+		fmt.Sprintf("--config=%s", lvmdConfigFile),
+		"--container=true",
+	}
+
+	resourceRequirements := corev1.ResourceRequirements{
+		Limits: corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse(TopolvmNodeCPULimit),
+			corev1.ResourceMemory: resource.MustParse(TopolvmNodeMemLimit),
+		},
+		Requests: corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse(TopolvmNodeCPURequest),
+			corev1.ResourceMemory: resource.MustParse(TopolvmNodeMemRequest),
+		},
+	}
+
+	volumeMounts := []corev1.VolumeMount{
+		{Name: "lvmd-socket-dir", MountPath: "/run/topolvm"},
+		{Name: "lvmd-config-dir", MountPath: "/etc/topolvm"}}
+
+	privilege := true
+	runUser := int64(0)
+	lvmd := &corev1.Container{
+		Name:  "lvmd",
+		Image: TopolvmCsiImage,
+		SecurityContext: &corev1.SecurityContext{
+			Privileged: &privilege,
+			RunAsUser:  &runUser,
+		},
+		Command:      command,
+		Resources:    resourceRequirements,
+		VolumeMounts: volumeMounts,
+	}
+	return lvmd
+}
+
+func getNodeContainer() *corev1.Container {
+	privileged := true
+	runAsUser := int64(0)
+
+	command := []string{
+		"/topolvm-node",
+		"--lvmd-socket=/run/lvmd/lvmd.sock",
+	}
+
+	requirements := corev1.ResourceRequirements{
+		Limits: corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse(TopolvmNodeCPULimit),
+			corev1.ResourceMemory: resource.MustParse(TopolvmNodeMemLimit),
+		},
+		Requests: corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse(TopolvmNodeCPURequest),
+			corev1.ResourceMemory: resource.MustParse(TopolvmNodeMemRequest),
+		},
+	}
+
+	mountPropagationMode := corev1.MountPropagationBidirectional
+
+	volumeMounts := []corev1.VolumeMount{
+		{Name: "node-plugin-dir", MountPath: "/run/topolvm"},
+		{Name: "lvmd-socket-dir", MountPath: "/run/lvmd"},
+		{Name: "pod-volumes-dir",
+			MountPath:        fmt.Sprintf("%spods", getAbsoluteKubeletPath(CSIKubeletRootDir)),
+			MountPropagation: &mountPropagationMode},
+		{Name: "csi-plugin-dir",
+			MountPath:        fmt.Sprintf("%splugins/kubernetes.io/csi", getAbsoluteKubeletPath(CSIKubeletRootDir)),
+			MountPropagation: &mountPropagationMode},
+	}
+
+	env := []corev1.EnvVar{
+		{Name: "NODE_NAME",
+			ValueFrom: &corev1.EnvVarSource{
+				FieldRef: &corev1.ObjectFieldSelector{
+					FieldPath: "spec.nodeName"}}}}
+
+	node := &corev1.Container{
+		Name:    NodeContainerName,
+		Image:   TopolvmCsiImage,
+		Command: command,
+		SecurityContext: &corev1.SecurityContext{
+			Privileged: &privileged,
+			RunAsUser:  &runAsUser,
+		},
+		Ports: []corev1.ContainerPort{{Name: TopolvmNodeContainerHealthzName,
+			ContainerPort: 9808,
+			Protocol:      corev1.ProtocolTCP}},
+		LivenessProbe: &corev1.Probe{
+			Handler: corev1.Handler{
+				HTTPGet: &corev1.HTTPGetAction{Path: "/healthz",
+					Port: intstr.FromString(TopolvmNodeContainerHealthzName)}},
+			FailureThreshold:    3,
+			InitialDelaySeconds: 10,
+			TimeoutSeconds:      3,
+			PeriodSeconds:       60},
+		Resources:    requirements,
+		Env:          env,
+		VolumeMounts: volumeMounts,
+	}
+	return node
+}
+
+func getCsiRegistrarContainer() *corev1.Container {
+	command := []string{
+		"/csi-node-driver-registrar",
+		"--csi-address=/run/topolvm/csi-topolvm.sock",
+		fmt.Sprintf("--kubelet-registration-path=%splugins/topolvm.cybozu.com/node/csi-topolvm.sock", getAbsoluteKubeletPath(CSIKubeletRootDir)),
+	}
+
+	volumeMounts := []corev1.VolumeMount{
+		{Name: "node-plugin-dir", MountPath: "/run/topolvm"},
+		{Name: "registration-dir", MountPath: "/registration"},
+	}
+
+	preStopCmd := []string{
+		"/bin/sh",
+		"-c",
+		"rm -rf /registration/topolvm.cybozu.com /registration/topolvm.cybozu.com-reg.sock",
+	}
+
+	csiRegistrar := &corev1.Container{
+		Name:         "csi-registrar",
+		Image:        CsiRegistrarImage,
+		Command:      command,
+		Lifecycle:    &corev1.Lifecycle{PreStop: &corev1.Handler{Exec: &corev1.ExecAction{Command: preStopCmd}}},
+		VolumeMounts: volumeMounts,
+	}
+	return csiRegistrar
+}
+
+func getNodeLivenessProbeContainer() *corev1.Container {
+	command := []string{
+		"/livenessprobe",
+		"--csi-address=/run/topolvm/csi-topolvm.sock",
+	}
+
+	volumeMounts := []corev1.VolumeMount{
+		{Name: "node-plugin-dir", MountPath: "/run/topolvm"},
+	}
+
+	liveness := &corev1.Container{
+		Name:         "liveness-probe",
+		Image:        CsiLivenessProbeImage,
+		Command:      command,
+		VolumeMounts: volumeMounts,
+	}
+	return liveness
+}
+
+func getAbsoluteKubeletPath(name string) string {
+	if strings.HasSuffix(name, "/") {
+		return name
+	} else {
+		return name + "/"
+	}
+}
