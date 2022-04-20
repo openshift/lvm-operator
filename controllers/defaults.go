a38c27749e026a3e95f5d59aa87df90d013cbedc
Santosh Pillai <sapillai@redhat.com>
2022-04-20 16:30:08 +0530

    fix: update topolvm csi image to 0.11.0
    Topolvm image was updated to 0.11.0 only in go.mod and makefile.
    This PR updates the image version in all the required places.
    
    Signed-off-by: Santosh Pillai <sapillai@redhat.com>

--- controllers/defaults.go
+++ controllers/defaults.go
@@ -23,7 +23,7 @@ import (
 var (
 	defaultValMap = map[string]string{
 		"OPERATOR_NAMESPACE":      "openshift-storage",
-		"TOPOLVM_CSI_IMAGE":       "quay.io/topolvm/topolvm:0.10.3",
+		"TOPOLVM_CSI_IMAGE":       "quay.io/topolvm/topolvm:0.11.0",
 		"CSI_REGISTRAR_IMAGE":     "k8s.gcr.io/sig-storage/csi-node-driver-registrar:v2.3.0",
 		"CSI_PROVISIONER_IMAGE":   "k8s.gcr.io/sig-storage/csi-provisioner:v3.0.0",
 		"CSI_LIVENESSPROBE_IMAGE": "k8s.gcr.io/sig-storage/livenessprobe:v2.5.0",
