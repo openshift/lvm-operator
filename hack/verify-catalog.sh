757a6fe22e08ca0b6755eaee18b834dfe62795e7
Jakob Möller <jmoller@redhat.com>
2023-10-20 17:05:07 +0200

    feat: migrate to FBC for catalog in makefile
    Signed-off-by: Jakob Möller <jmoller@redhat.com>

---
+++ hack/verify-catalog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+function print_failure {
+    echo "There are unexpected changes to the tree when running 'make catalog-render'. Please"
+    echo "run these commands locally and double-check the Git repository for unexpected changes which may"
+    echo "need to be committed."
+    exit 1
+}
+
+if [ "${OPENSHIFT_CI:-false}" = true ]; then
+    echo "> generating the OLM catalog"
+    make catalog-render
+
+    test -z "$(git status --porcelain | \grep -v '^??')" || print_failure
+    echo "> verified generated catalog"
+fi
