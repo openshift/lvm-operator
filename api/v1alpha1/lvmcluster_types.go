b8fe49e83b4b736092274864ad601b675704b66b
Suleyman Akbas <sakbas@redhat.com>
2024-05-15 14:03:17 +0200

    feat: add conditions to LVMCluster
    Signed-off-by: Suleyman Akbas <sakbas@redhat.com>

--- api/v1alpha1/lvmcluster_types.go
+++ api/v1alpha1/lvmcluster_types.go
@@ -121,6 +121,8 @@ const (
 	LVMStatusFailed LVMStateType = "Failed"
 	// LVMStatusDegraded means that the lvmcluster has been created but is not using the specified config
 	LVMStatusDegraded LVMStateType = "Degraded"
+	// LVMStatusUnknown means that the lvmcluster has been in an unknown state
+	LVMStatusUnknown LVMStateType = "Unknown"
 )
 
 // LVMClusterStatus defines the observed state of LVMCluster
@@ -138,55 +140,18 @@ type LVMClusterStatus struct {
 
 	// Conditions describes the state of the resource.
 	// +optional
-	Conditions []Condition `json:"conditions,omitempty"`
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 
 	// DeviceClassStatuses describes the status of all deviceClasses
 	DeviceClassStatuses []DeviceClassStatus `json:"deviceClassStatuses,omitempty"`
 }
 
-// Condition represents the state of the operator's
-// reconciliation functionality.
-// +k8s:deepcopy-gen=true
-type Condition struct {
-	Type ConditionType `json:"type" description:"type of condition ie. Available|Progressing|Degraded."`
-
-	Status corev1.ConditionStatus `json:"status" description:"status of the condition, one of True, False, Unknown"`
-
-	// +optional
-	Reason string `json:"reason,omitempty" description:"one-word CamelCase reason for the condition's last transition"`
-
-	// +optional
-	Message string `json:"message,omitempty" description:"human-readable message indicating details about last transition"`
-
-	// +optional
-	LastHeartbeatTime metav1.Time `json:"lastHeartbeatTime" description:"last time we got an update on a given condition"`
-
-	// +optional
-	LastTransitionTime metav1.Time `json:"lastTransitionTime" description:"last time the condition transit from one status to another"`
-}
-
-// ConditionType is the state of the operator's reconciliation functionality.
-type ConditionType string
-
 const (
-	// ConditionAvailable indicates that the resources maintained by the operator,
-	// is functional and available in the cluster.
-	ConditionAvailable ConditionType = "Available"
+	// ResourcesAvailable indicates whether the resources maintained by the operator are reconciled without any issues.
+	ResourcesAvailable = "ResourcesAvailable"
 
-	// ConditionProgressing indicates that the operator is actively making changes to the resources maintained by the
-	// operator
-	ConditionProgressing ConditionType = "Progressing"
-
-	// ConditionDegraded indicates that the resources maintained by the operator are not functioning completely.
-	// An example of a degraded state would be if not all pods in a deployment were running.
-	// It may still be available, but it is degraded
-	ConditionDegraded ConditionType = "Degraded"
-
-	// ConditionUpgradeable indicates whether the resources maintained by the operator are in a state that is safe to upgrade.
-	// When `False`, the resources maintained by the operator should not be upgraded and the
-	// message field should contain a human readable description of what the administrator should do to
-	// allow the operator to successfully update the resources maintained by the operator.
-	ConditionUpgradeable ConditionType = "Upgradeable"
+	// VolumeGroupsReady indicates whether the volume groups maintained by the operator are in a ready state.
+	VolumeGroupsReady = "VolumeGroupsReady"
 )
 
 // DeviceClassStatus defines the observed status of the deviceclass across all nodes
