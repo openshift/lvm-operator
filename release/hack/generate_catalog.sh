31d333d8243ff54ec1bbda8968e65a65f731d834
Jeff Roche <82384784+jeff-roche@users.noreply.github.com>
2025-10-08 08:47:28 -0400

    feat: onboarding 4.16 catalog to release-4.16 branch (#1494)
    Signed-off-by: Jeff Roche <jeroche@redhat.com>

---
+++ release/hack/generate_catalog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+opm_path="${1}"
+
+template_file="release/catalog/lvm-operator-catalog-template.yaml"
+
+echo "Generating catalog based on ${template_file}"
+output_file="release/catalog/lvm-operator-catalog.json"
+
+catalog=$(${opm_path} alpha render-template semver -o yaml ${template_file})
+
+# The semver render-template function by default names the channels "stable-vX.Y" but we historically don't use the "v" in our channel names
+patched_catalog=$(echo "${catalog}" | yq -o=json eval-all '(.. | select(tag == "!!str")) |= sub("stable-v", "stable-") | (.. | select(tag == "!!str")) |= sub("candidate-v", "candidate-")')
+
+echo "${patched_catalog}" > ${output_file}
+
+echo "Catalog generated: ${output_file}"
