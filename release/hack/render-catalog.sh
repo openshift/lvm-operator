c0117379485dd4a2fd2ae15d51b9d2bb2ea85086
Jeff Roche <82384784+jeff-roche@users.noreply.github.com>
2025-11-11 12:04:45 -0500

    chore: updating the catalog build pipeline (#1670)
    Signed-off-by: Jeff Roche <jeroche@redhat.com>

---
+++ release/hack/render-catalog.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+catalog_file="release/catalog/lvm-operator-catalog.json"
+
+catalog_content=$(cat ${catalog_file})
+
+# Channel name patching to match legacy format (no v)
+echo "Patching channel names to match legacy format"
+catalog_content="${catalog_content//stable-v/stable-}"
+
+# Set the skip range for the latest version of the operator
+channel=$(echo "${catalog_content}" | yq -p=json 'select(.schema == "olm.package").defaultChannel')
+latest_version=$(echo "${catalog_content}" | ch="${channel}" yq -p=json 'select(.schema == "olm.channel" and .name == strenv(ch)).entries[-1].name' | cut -d. -f2-)
+latest_version=${latest_version:1} # Strip the leading v
+skip_range=">=4.2.0 <${latest_version}"
+
+echo "Setting the skip range for 'v${latest_version}' on channel ${channel} to '${skip_range}'"
+catalog_content=$(echo "${catalog_content}" | ch="${channel}" sr="${skip_range}" yq -p=json -o=json -e e 'select(.schema == "olm.channel" and .name == strenv(ch)).entries[-1].skipRange = strenv(sr)')
+
+echo "${catalog_content}" > $catalog_file
+echo "Catalog patched at ${catalog_file}"
