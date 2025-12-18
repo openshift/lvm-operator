176eadb718175a0c073cfdf1c131b4c28497f6c2
Jeff Roche <82384784+jeff-roche@users.noreply.github.com>
2025-12-18 09:23:17 -0500

    OCPEDGE-2288: Release 4.17.7 (#1844)
    * rel: v4.17.7
    
    Signed-off-by: Jeff Roche <jeroche@redhat.com>
    
    * fix: added error handling to the release automation script
    
    Signed-off-by: Jeff Roche <jeroche@redhat.com>
    
    ---------
    
    Signed-off-by: Jeff Roche <jeroche@redhat.com>

--- hack/manage-release.sh
+++ hack/manage-release.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
-: ${PULL_BASE_SHA:=""}
+set -euo pipefail
+: ${PULL_NUMBER:=""}
 : ${REPO_OWNER:="openshift"}
 : ${REPO_NAME:="lvm-operator"}
 : ${KONFLUX_SA_TOKEN:=""}
 : ${KONFLUX_SERVER:=""}
-: ${KONFLUX_PROJECT:=""}
 : ${DRYRUNONLY:=""}
 
-commit_data=$(curl -s -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/commits/${PULL_BASE_SHA}")
-changes=$(echo "${commit_data}" | jq '.files[] | select(.status == "added") | select(.filename|test("releases/*")) | {"changes": [.filename]}')
+commit_data=$(curl -s -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls/${PULL_NUMBER}/files")
+changes=$(echo "${commit_data}" | jq '{"changes": [.[] | select(.filename|test("releases/*")) | .filename]}')
+
+echo "Changes detected:"
+echo "${changes}" | jq ".changes[]"
+
+KUBECONFIG="./konflux-config"
+
+oc="oc --kubeconfig='${KUBECONFIG}'"
 
 # Login to the Konflux Server
-oc login --token=${KONFLUX_SA_TOKEN} --server=${KONFLUX_SERVER}
+if ! $oc login --token="${KONFLUX_SA_TOKEN}" --server="${KONFLUX_SERVER}"; then
+    status=$?
+    echo "Error: Unable to login with the provided token. Exiting.."
+    exit 1
+fi
+
+$oc project "logical-volume-manag-tenant"
 
 echo "Do I have appropriate permissions to do a release?"
-oc auth can-i create releases --namespace=${KONFLUX_PROJECT}
+perms=$($oc auth can-i create releases)
+if ! [ $? ] || [ "${perms}" == "no" ]; then
+    echo "Error: The auth token does not have permissions to create a release."
+    exit 1
+fi
 
-echo "Applying release configurations to the ${KONFLUX_PROJECT} project on the ${KONFLUX_SERVER} server."
+echo "Applying release configurations"
 
 dry_run_flag=""
 if ! [ -z "${DRYRUNONLY}" ]; then
@@ -25,5 +42,9 @@ fi
 
 for change in $(echo "${changes}" | jq '.changes[]'); do
     echo "Applying ${change}"
-    oc --namespace=${KONFLUX_PROJECT} apply $dry_run_flag -f $change
+    if ! $oc apply $dry_run_flag -f "${change}" -o yaml; then
+        status=$?
+        echo "Error: Unable to apply ${change}. Return code: ${status}"
+        exit 1
+    fi
 done
