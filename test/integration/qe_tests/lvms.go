bad59c3c73276a0444f1f11f8761cd53f1907d91
Kasturi Narra <knarra@redhat.com>
2026-08-19 15:35:31 +0530

    Skip tests 73363 & 67002

--- test/integration/qe_tests/lvms.go
+++ test/integration/qe_tests/lvms.go
@@ -1121,6 +1121,7 @@ var _ = g.Describe("[sig-storage] STORAGE", func() {
 	g.It("Author:rdeore-High-67002-[OTP][LVMS] Check deviceSelector logic works with only optional paths [Disruptive]", g.Label("MNO", "Serial"), func() {
 
 		g.By("Get list of available block devices/disks attached to all worker nodes")
+		g.Skip("Temporarily disabled - OCPEDGE-2937: vg-manager retains stale devices after recreation")
 		freeDiskNameCountMap, err := getListOfFreeDisksFromWorkerNodes(tc)
 		o.Expect(err).NotTo(o.HaveOccurred())
 
@@ -2494,7 +2495,7 @@ spec:
 	})
 
 	g.It("Author:rdeore-High-73363-[OTP][LVMS] Check hot reload of lvmd configuration is working [Disruptive]", g.Label("SNO", "MNO", "Serial"), func() {
-
+		g.Skip("Temporarily disabled - OCPEDGE-2936: CSIStorageCapacity stale after LVMCluster recreation")
 		var (
 			storageCapacity    int
 			lvmdConfigFilePath = "/etc/topolvm/lvmd.yaml"
